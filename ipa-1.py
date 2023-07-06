0d879855d378569ab9037e565138ab7bb7131c0f
nathanbata <136476981+nathanbata@users.noreply.github.com>
2023-07-06 11:43:52 +0800

    Add files via upload

---
+++ ipa-1.py
@@ -0,0 +1,174 @@
+'''Individual Programming Assignment 1
+
+20 points
+
+This assignment will develop your basic familiarity with Python.
+'''
+
+def savings(gross_pay, tax_rate, expenses):
+    '''Savings.
+    5 points.
+
+    This function calculates the money remaining
+        for an employee after taxes and expenses.
+
+    To get the take-home pay of an employee, we will
+        follow the following process:
+        1. Apply the tax rate to the gross pay of the employee; round down
+        2. Subtract the expenses from the after-tax pay of the employee
+
+    Parameters
+    ----------
+    gross_pay: int
+        the gross pay of an employee for a certain time period, expressed in centavos
+    tax_rate: float
+        the tax rate for a certain time period, expressed as a number between 0 and 1 (e.g., 0.12)
+    expenses: int
+        the expenses of an employee for a certain time period, expressed in centavos
+
+    Returns
+    -------
+    int
+        the number of centavos remaining from an employee's pay after taxes and expenses
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    tax_expense = gross_pay * tax_rate
+    total_expense = tax_expense + expenses
+    personal_savings = int((gross_pay - total_expense)/100)
+    return(f"Your total savings is PHP {personal_savings}.")
+
+print("With figures coming from the same time period,:")
+gross_pay = int(input("Enter your gross income in PHP:"))*100
+tax_rate = float(input("Enter your tax rate in decimal form:"))
+expenses = int(input("Enter your total expenses, excluding taxes in PHP:"))*100
+
+savings(gross_pay, tax_rate, expenses)
+
+def material_waste(total_material, material_units, num_jobs, job_consumption):
+    '''Material Waste.
+    5 points.
+
+    This function calculates how much material input will be wasted
+        after running a certain number of jobs that consume
+        a set amount of material.
+
+    To get the waste of a set of jobs:
+        1. Multiply the number of jobs by the material consumption per job.
+        2. Subtract the total material consumed from the total material available.
+
+    The users of this function also want you to format the output as a string, annotated with the
+        units in which the material is expressed. Do not add a space between the number and the unit.
+
+    Parameters
+    ----------
+    total_material: int
+        the total material available
+    material_units: str
+        the units used to express a quantity of the material (e.g., "kg", "L", etc.)
+    num_jobs: int
+        the number of jobs to run
+    job_consumption: int
+        the amount of material consumed per job
+
+    Returns
+    -------
+    str
+        the amount of remaining material expressed with its unit (e.g., "10kg").
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    total_usage = num_jobs * job_consumption
+    total_waste = total_material - total_usage
+    return(f"The total material wastage for these jobs is {total_waste}{material_units}.")
+
+total_material = int(input("Enter the total material available, without units:"))
+material_units = str(input("Enter the unit of measurement for the material:"))
+num_jobs = int(input("Enter the number of jobs to run:"))
+job_consumption = int(input("Enter the amount of material consumed per job, without units:"))
+
+material_waste(total_material, material_units, num_jobs, job_consumption)
+
+def interest(principal, rate, periods):
+    '''Interest.
+    5 points.
+
+    This function calculates the final value of an investment after
+        gaining simple interest over a number of periods.
+
+    To calculate simple interest, simply multiply the principal to the quantity (rate * time).
+        Add this amount to the principal to get the final value.
+
+    Round down the final amount.
+
+    Parameters
+    ----------
+    principal: int
+        the principal (i.e., starting) amount invested, expressed in centavos
+    rate: float
+        the interest rate per period, expressed as a decimal representation of a percentage (e.g., 3% is 0.03)
+    periods: int
+        the number of periods invested
+
+    Returns
+    -------
+    int
+        the final value of the investment
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    roi = (periods * rate) * principal
+    final_value = int((roi + principal))/100
+    return(f"The final value of the investment is PHP{final_value}, rounded down to the nearest integer.")
+
+principal = int(input("Enter the principal amount here, without units:"))*100
+rate = float(input("Enter the investment rate per period, expressed as a decimal:"))
+periods = int(input("Enter the number of periods in the investment:"))
+
+interest(principal, rate, periods)
+
+def body_mass_index(weight, height):
+    '''Body Mass Index.
+    5 points.
+
+    This function calculates the body mass index (BMI) of a person
+        given their weight and height.
+
+    The formula for BMI is: kg / (m ^ 2)
+        (i.e., kilograms over meters squared)
+
+    Unfortunately, the users of this function use the imperial system.
+        You will need to first convert their arguments to the metric system.
+
+    Parameters
+    ----------
+    weight: float
+        the weight of the person, in pounds
+    height: list
+        the height of the person, expressed as a list of two integers.
+        the first integer is the foot component of their height.
+        the second integer is the inches component of their height.
+        for example, 5'10" would be passed as [5, 10].
+
+    We have not yet discussed lists, but use the skills you developed
+        in the command line exercise. How would you learn how to work with
+        lists?
+
+    Returns
+    -------
+    float
+        the BMI of the person.
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    height_list = []
+    height_list.append(height[0])
+    height_list.append(height[height.index("'") + 1:height.index("\"")])
+    weight_kilos = weight / 2.205
+    height_meters = (int(height_list[0])*12 + int(height_list[1])) / 39.37
+    bmi = weight_kilos / (height_meters ** 2)
+    return(f"Your BMI is {bmi}.")
+
+weight = float(input("Enter your weight, in pounds:"))
+height = str(input('''Enter your height, in feet and inches in the format - feet\'inches":'''))
+body_mass_index(weight, height)
